1368fb2c9e933aa388ab8dfd0b3c5e74456193cb
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-02-25 21:30:13 +0530

    Create snake part

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -11,7 +11,19 @@ FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
 
 class Snake:
-    pass
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
+
+
 
 class Food:
     def __init__(self):
